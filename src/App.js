6b7ee19ae28d0a58ff8e5c00ddc660eb1d341aaa
AdrianCo97 <adrian.corazza@outlook.com>
2021-09-24 16:23:49 +0200

    Made a time textfield.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import Button from "@material-ui/core/Button";
-import ButtonGroup from "@material-ui/core/ButtonGroup"
+import ButtonGroup from "@material-ui/core/ButtonGroup";
+import TextField from "@material-ui/core/TextField"
+
 import SaveIcon from "@material-ui/icons/Save"
 import DeleteIcon from "@material-ui/icons/Delete"
+
+import "./App.css"
+
 
 
 function App() {
   return (
-    <div className="App">
+    
+    <div className="content">
+      <TextField 
+        variant="filled"
+        type="time"
+        label="The time"
+      />
       <ButtonGroup>
       <Button variant="contained" size="large" color="primary" startIcon={<SaveIcon />} >
         Save
