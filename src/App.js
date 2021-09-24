fd5334f93fdf72325a8b03df98543c28866d633f
AdrianCo97 <adrian.corazza@outlook.com>
2021-09-24 15:22:50 +0200

    Created a save button

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import Button from "@material-ui/core/Button";
-
+import SaveIcon from "@material-ui/icons/Save"
 
 function App() {
   return (
     <div className="App">
-      <Button variant="contained" size="large">
-        This is a Material-UI button
+      <Button variant="contained" size="large" startIcon={<SaveIcon />}>
+        Save
       </Button>
     </div>
   );
