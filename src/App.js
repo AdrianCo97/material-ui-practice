3ec13f3569d829913b28cef8c01928a24f077336
AdrianCo97 <adrian.corazza@outlook.com>
2021-09-24 15:30:32 +0200

    Created a deletebutton and grouped it in a buttongroup

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import Button from "@material-ui/core/Button";
+import ButtonGroup from "@material-ui/core/ButtonGroup"
 import SaveIcon from "@material-ui/icons/Save"
+import DeleteIcon from "@material-ui/icons/Delete"
+
 
 function App() {
   return (
     <div className="App">
-      <Button variant="contained" size="large" startIcon={<SaveIcon />}>
+      <ButtonGroup>
+      <Button variant="contained" size="large" color="primary" startIcon={<SaveIcon />} >
         Save
       </Button>
+
+      <Button variant="contained" size="large" color="secondary" startIcon={<DeleteIcon />}>
+        Delete
+      </Button>
+      </ButtonGroup>
     </div>
   );
 }
