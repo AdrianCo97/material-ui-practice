216fb30875dcdf48af1f79bf3c3da54f8bfdae6e
AdrianCo97 <adrian.corazza@outlook.com>
2021-09-24 15:03:04 +0200

    Created a basic button

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
-
+import Button from "@material-ui/core/Button";
 
 function App() {
   return (
     <div className="App">
+      <Button variant="contained" color="secondary">
+        This is a Material-UI button
+      </Button>
     </div>
   );
 }
